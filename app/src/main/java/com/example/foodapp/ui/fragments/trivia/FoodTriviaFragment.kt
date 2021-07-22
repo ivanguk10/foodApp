5b214fd8c36bed8990581fd987b57f91ad7b5257
ivanguk10 <48579825+ivanguk10@users.noreply.github.com>
2021-07-22 10:09:52 +0300

    Share Food Trivia

--- app/src/main/java/com/example/foodapp/ui/fragments/trivia/FoodTriviaFragment.kt
+++ app/src/main/java/com/example/foodapp/ui/fragments/trivia/FoodTriviaFragment.kt
@@ -1,11 +1,10 @@
 package com.example.foodapp.ui.fragments.trivia
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -25,6 +24,8 @@ class FoodTriviaFragment : Fragment() {
 
     private val mainViewModel: MainViewModel by viewModels()
 
+    private var foodTrivia = "No Food Trivia"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,12 +36,15 @@ class FoodTriviaFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.mainViewModel = mainViewModel
 
+        setHasOptionsMenu(true)
+
         mainViewModel.getTrivia(API_KEY)
         mainViewModel.triviaResponse.observe(viewLifecycleOwner, { response ->
             when(response) {
                 is NetworkResult.Success -> {
                     binding.foodTriviaTextView.text = response.data?.text
                     Log.d("foodJokeFragment", "ok")
+                    foodTrivia = response.data!!.text
                 }
                 is NetworkResult.Error -> {
                     Toast.makeText(requireContext(), response.message.toString(), Toast.LENGTH_SHORT).show()
@@ -62,9 +66,26 @@ class FoodTriviaFragment : Fragment() {
             mainViewModel.readFoodTrivia.observe(viewLifecycleOwner, { database ->
                 if (database.isNotEmpty() && database != null) {
                     binding.foodTriviaTextView.text = database[0].foodTrivia.text
+                    foodTrivia = database[0].foodTrivia.text
                 }
             })
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.share_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.share) {
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT, foodTrivia)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
